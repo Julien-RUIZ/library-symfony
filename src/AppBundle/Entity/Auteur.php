1a17cbed249a2673bea6717d9a62d813c78d3a37
lapiscine <julien.ruiz@lapiscine.pro>
2018-11-26 08:16:40 +0100

    master

---
+++ src/AppBundle/Entity/Auteur.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lapiscine
+ * Date: 21/11/2018
+ * Time: 14:50
+ */
+
+//identifier une classe
+namespace AppBundle\Entity;
+
+
+//on vient importer le namespace en lui donnant un alias, donc on l'apperlera ORM
+use Doctrine\ORM\Mapping as ORM;
+
+//technique en annotation, ainsi on dit que la classe auteur est une entity et on lui donne un nom a la table
+    /**
+     * @ORM\Entity
+     * @ORM\Table(name="auteur")
+     * @ORM\Entity(repositoryClass="AppBundle\Repository\AuteurRepository")
+     */
+    class Auteur
+    {
+    //colonne type number, avec auto incrementation
+        /**
+         * @ORM\Column(type="integer")
+         * @ORM\Id
+         * @ORM\GeneratedValue(strategy="AUTO")
+         */
+        private $id;
+
+
+        /**
+         * @ORM\Column(type="string")
+         */
+        private $nom;
+
+        /**
+         * @return mixed
+         */
+        public function getNom()
+        {
+            return $this->nom;
+        }
+
+        /**
+         * @param mixed $nom
+         */
+        public function setNom($nom)
+        {
+            $this->nom = $nom;
+        }
+
+        /**
+         * @ORM\Column(type="date", name="date_naissance")
+         */
+        private $dateNaissance;
+
+        /**
+         * @ORM\Column(type="string")
+         */
+        private $pays;
+
+        /**
+         * @return mixed
+         */
+        public function getPays()
+        {
+            return $this->pays;
+        }
+
+        /**
+         * @param mixed $pays
+         */
+        public function setPays($pays)
+        {
+            $this->pays = $pays;
+        }
+
+        /**
+         * @ORM\Column(type="date", name="date_de_mort")
+         */
+        private $dateMort;
+
+        /**
+         * @return mixed
+         */
+        public function getDateNaissance()
+        {
+            return $this->dateNaissance;
+        }
+
+        /**
+         * @param mixed $dateNaissance
+         */
+        public function setDateNaissance($dateNaissance)
+        {
+            $this->dateNaissance = $dateNaissance;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getBiographie()
+        {
+            return $this->biographie;
+        }
+
+        /**
+         * @param mixed $biographie
+         */
+        public function setBiographie($biographie)
+        {
+            $this->biographie = $biographie;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDateMort()
+        {
+            return $this->dateMort;
+        }
+
+        /**
+         * @param mixed $dateMort
+         */
+        public function setDateMort($dateMort)
+        {
+            $this->dateMort = $dateMort;
+        }
+
+
+        /**
+         * @ORM\Column(type="text")
+         */
+        private $biographie;
+
+        /**
+         * @return mixed
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+
+
+
+    }
---
+++ src/AppBundle/Entity/Livre.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Livre
+ *
+ * @ORM\Table(name="livre")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\LivreRepository")
+ */
+class Livre
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string", length=255)
+     */
+    private $genre;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nbPages", type="integer")
+     */
+    private $nbPages;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="format", type="string", length=255)
+     */
+    private $format;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $auteur;
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * @param mixed $auteur
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+    }
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set genre
+     *
+     * @param string $genre
+     *
+     * @return Livre
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Get genre
+     *
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set nbPages
+     *
+     * @param integer $nbPages
+     *
+     * @return Livre
+     */
+    public function setNbPages($nbPages)
+    {
+        $this->nbPages = $nbPages;
+
+        return $this;
+    }
+
+    /**
+     * Get nbPages
+     *
+     * @return int
+     */
+    public function getNbPages()
+    {
+        return $this->nbPages;
+    }
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     *
+     * @return Livre
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+}
+
